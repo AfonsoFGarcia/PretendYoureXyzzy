8da93d5b8e2ce7bc2fcf076863912e599fe9494a
Afonso F. Garcia <afonsogarcia@icloud.com>
2015-03-08 12:56:02 +0000

    Finished reset db script

--- reset_db.sh
+++ reset_db.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-set -e
 
+sudo service tomcat7 stop
+export PGPASSWORD=12345
 gcc -o cardparser cardparser.c
 ./cardparser blackcards.txt whitecards.txt twitter.sql
 sudo -u postgres ./drop.sh
 psql -h localhost -d cahdb -U cah -f cah_cards.sql
 psql -h localhost -d cahdb -U cah -f twitter.sql
+sudo service tomcat7 start
